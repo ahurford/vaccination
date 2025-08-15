7099aae2bcc542cb5f0719c0d49735064600e410
Amy Hurford <ahurford@mun.ca>
2025-08-15 10:40:18 -0230

    Update vaccination-script.R

--- vaccination-script.R
+++ vaccination-script.R
@@ -4,3 +4,4 @@ data = read.csv("https://raw.githubusercontent.com/ahurford/vaccination/refs/hea
 data1 = data%>%filter(prename=="Newfoundland and Labrador")%>%select("week_end", "numtotal_partially","numtotal_fully", "numtotal_additional", "numtotal_atleast1dose")%>%filter(week_end=="2021-12-18")
 
 data1$numtotal_partially+data1$numtotal_fully
+
