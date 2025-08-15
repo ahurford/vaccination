825a8a9c6549b9c7f676c25af85fea020e6fc1d7
Amy Hurford <ahurford@mun.ca>
2025-08-14 22:09:43 -0230

    Create vaccination-script.R

---
+++ vaccination-script.R
@@ -0,0 +1,6 @@
+require(dplyr)
+data = read.csv("https://raw.githubusercontent.com/ahurford/vaccination/refs/heads/main/vaccination-coverage-map.csv")
+
+data1 = data%>%filter(prename=="Newfoundland and Labrador")%>%select("week_end", "numtotal_partially","numtotal_fully", "numtotal_additional", "numtotal_atleast1dose")%>%filter(week_end=="2021-12-18")
+
+data1$numtotal_partially+data1$numtotal_fully
